37402d2a89a6f4a6e6092e3ff5d831551c93285c
Miatum <443398600@qq.com>
2022-02-25 02:26:34 +0800

    login page

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <span>username</span>
+        <input type="text" v-bind="username">
+      </li>
+      <li>
+        <span>password</span>
+        <input type="password" v-bind="password">
+      </li>
+      <li>
+        <input type="button" value="login" @click = "logIn">
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      dataServer: this.serverConfig.DataServer,
+      username: '',
+      password: ''
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    logIn: function () {
+      this.axios.get('/api/user/login', {
+        params: {
+          username: this.username,
+          password: this.password
+        }
+      }).then(response => {
+        console.log(response.data)
+        if (response.data.code == 200) {
+          //  成功登录跳转页面
+        } else {
+          console.log('登录失败')
+        }
+      }).catch(error =>{
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
